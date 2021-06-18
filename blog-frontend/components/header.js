f373fc0f7c4e845a91a12b7a7418767758c3cda7
J.C <jimbomoso@gmail.com>
2021-06-18 16:30:11 -0400

    Updates classnames

--- blog-frontend/components/header.js
+++ blog-frontend/components/header.js
@@ -40,14 +40,14 @@ export default function Header() {
         </ul>
       </div> */}
       <nav>
-        <div class="container mx-auto px-6 py-2 flex justify-between items-center">
-          <a class="font-bold text-2xl lg:text-4xl" href="#">
+        <div className="container mx-auto px-6 py-2 flex justify-between items-center">
+          <a className="font-bold text-2xl lg:text-4xl" href="#">
             NERR
           </a>
-          <div class="block lg:hidden">
+          <div className="block lg:hidden">
             <button class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
               <svg
-                class="fill-current h-3 w-3"
+                className="fill-current h-3 w-3"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -56,29 +56,29 @@ export default function Header() {
               </svg>
             </button>
           </div>
-          <div class="hidden lg:block">
-            <ul class="inline-flex">
+          <div className="hidden lg:block">
+            <ul className="inline-flex">
               <li>
-                <a class="px-4 font-bold" href="/">
+                <a className="px-4 font-bold" href="/">
                   Home
                 </a>
               </li>
               <li>
                 <Link href="/events">
-                <a class="px-4 hover:text-gray-800">
+                <a className="px-4 hover:text-gray-800">
                   Events
                 </a>
                 </Link>
               </li>
               <li>
                 <Link href="/about">
-                <a class="px-4 hover:text-gray-800">
+                <a className="px-4 hover:text-gray-800">
                   About
                 </a>
                 </Link>
               </li>
               <li>
-                <a class="px-4 hover:text-gray-800">
+                <a className="px-4 hover:text-gray-800">
                   Contact
                 </a>
               </li>
--- blog-frontend/components/homehero.js
+++ blog-frontend/components/homehero.js
@@ -3,11 +3,11 @@ import rodrun4 from "../public/rodrun4.jpg";
 
 export default function Homehero() {
   return (
-      <div class="relative">
-          <div class="absolute inset-0 z-10 flex justify-center items-end p-10 text-6xl text-white font-semibold">
+      <div className="relative">
+          <div className="absolute inset-0 z-10 flex justify-center items-end p-10 text-6xl text-white font-semibold">
             <p> Welcome to Northeast Rod Run</p>
           </div>
-        <img class="md:w-full lg:w-full" src={rodrun4} />
+        <img className="md:w-full lg:w-full" src={rodrun4} />
       </div>
   );
 }
