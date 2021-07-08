acae3a61bf503e59041c6514f425a1265095820c
J.C <jimbomoso@gmail.com>
2021-07-08 18:46:00 -0400

    Fixes home image

--- blog-frontend/components/homehero.js
+++ blog-frontend/components/homehero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from 'next/image'
 import rodrun4 from "../public/rodrun4.jpg";
 
 export default function Homehero() {
@@ -7,7 +7,7 @@ export default function Homehero() {
           <div className="absolute inset-0 z-10 flex justify-center items-end p-10 text-6xl text-white font-semibold">
             <p> Welcome to Northeast Rod Run</p>
           </div>
-        <img className="md:w-full lg:w-full" src={rodrun4} />
+        <Image className="md:w-full lg:w-full" src={rodrun4} />
       </div>
   );
 }
